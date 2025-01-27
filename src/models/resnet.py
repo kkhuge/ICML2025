ee4ac72ba2a4101afff396d5f0ecc7404a8879a9
kkhuge <115394186+kkhuge@users.noreply.github.com>
2025-01-27 16:14:35 +0800

    Update resnet.py

--- src/models/resnet.py
+++ src/models/resnet.py
@@ -3,14 +3,14 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-
+# 定义宽残差块
 class ResidualBlock(nn.Module):
     def __init__(self, in_channels, out_channels, stride=1, downsample=None):
         super(ResidualBlock, self).__init__()
         self.bn1 = nn.BatchNorm2d(in_channels)
-        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1)
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=True)
         self.bn2 = nn.BatchNorm2d(out_channels)
-        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=True)
         self.downsample = downsample
 
 
@@ -29,18 +29,20 @@ class ResidualBlock(nn.Module):
         return out
 
 
-
+# 定义 ResNet 主结构
 class ResNet(nn.Module):
     def __init__(self, block, k=None, num_classes=10):
         super(ResNet, self).__init__()
         self.in_channels = 16
-        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1)
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1, bias=True)
 
+        # 构建 conv2, conv3, conv4，每层宽度增加 k 倍
         self.layer1 = self._make_layer(block, 16 * k, 1)
         self.layer2 = self._make_layer(block, 16 * k, 1, stride=2)
         self.layer3 = self._make_layer(block, 16 * k, 1, stride=2)
+        self.bn1 = nn.BatchNorm2d(16*k)
 
-        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        # self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
         self.fc = nn.Linear(16 * k, num_classes)
 
 
@@ -48,8 +50,7 @@ class ResNet(nn.Module):
         downsample = None
         if stride != 1 or self.in_channels != out_channels:
             downsample = nn.Sequential(
-                nn.Conv2d(self.in_channels, out_channels, kernel_size=1, stride=stride),
-                nn.BatchNorm2d(out_channels)
+                nn.Conv2d(self.in_channels, out_channels, kernel_size=1, stride=stride, bias=True),
             )
 
         layers = []
@@ -66,9 +67,9 @@ class ResNet(nn.Module):
         x = self.layer1(x)
         x = self.layer2(x)
         x = self.layer3(x)
-
-        x = self.avgpool(x)
-        x = torch.flatten(x, 1)
+        x = F.relu(self.bn1(x))
+        x = F.avg_pool2d(x, 8)
+        x = x.view(x.size(0), -1)
         x = self.fc(x)
         return x
 
@@ -80,6 +81,9 @@ def resnet_2(num_classes=10):
 
 def resnet_4(num_classes=10):
     return ResNet(ResidualBlock, k=4, num_classes=num_classes)
+
+def resnet_6(num_classes=10):
+    return ResNet(ResidualBlock, k=6, num_classes=num_classes)
 
 def resnet_8(num_classes=10):
     return ResNet(ResidualBlock, k=8, num_classes=num_classes)
