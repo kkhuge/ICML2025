5bef1575a12e74efdd58e32598646851a5b9581b
kkhuge <115394186+kkhuge@users.noreply.github.com>
2025-02-05 11:39:27 +0800

    Update fedavg5.py

--- src/trainers/fedavg5.py
+++ src/trainers/fedavg5.py
@@ -36,7 +36,6 @@ class FedAvg5Trainer(BaseTrainer):
         self.weight_change = []
         model = choose_model(options)
         self.move_model_to_gpu(model, options)
-        self.required_accuracy = options['psi']
         self.tau = options['num_epoch']
         self.optimizer = SGD(model.parameters(), lr=options['lr'], weight_decay=0.0001)
         self.num_epoch = options['num_epoch']
